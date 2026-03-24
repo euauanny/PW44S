4b0338c2a5410de4b5fea5626e34549a89ba68fc
Auanny <auannycomerlato@alunos.utfpr.edu.br>
2026-03-24 20:57:35 -0300

    commit

--- server/server/src/main/java/br/edu/utfpr/pb/pw44s/server/model/User.java
+++ server/server/src/main/java/br/edu/utfpr/pb/pw44s/server/model/User.java
@@ -1,13 +1,34 @@
 package br.edu.utfpr.pb.pw44s.server.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
+@Entity
+@Table(name = "tb_users")
 @Getter @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Size(min = 4, max = 50)
     private String username;
+
+    @NotNull
+    @Size(min = 4, max = 50)
     private String displayName;
+
+    @NotNull(message = "A senha nao pode ser nula")
+    @Size(min = 6, max = 20)
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$", message = "A senha deve conter pelo menos uma letra maiuscula, uma minuscula e um numero" )
     private String password;
+
 }
--- server/server/src/test/java/br/edu/utfpr/pb/pw44s/server/UserControllerTest.java
+++ server/server/src/test/java/br/edu/utfpr/pb/pw44s/server/UserControllerTest.java
@@ -1,13 +1,21 @@
 package br.edu.utfpr.pb.pw44s.server;
 
 import br.edu.utfpr.pb.pw44s.server.model.User;
+import br.edu.utfpr.pb.pw44s.server.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.resttestclient.TestRestTemplate;
 import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureRestTestClient;
 import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureTestRestTemplate;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.client.RestTestClient;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.result.StatusResultMatchersExtensionsKt.isEqualTo;
 
 
 @SpringBootTest(webEnvironment =
@@ -22,12 +30,12 @@ public class UserControllerTest {
     @Autowired
     private RestTestClient restTestClient;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Test
     public void postUser_whenUserIsValid_receiveCREATED() {
-        User user = User.builder()
-                .username("test-user")
-                .displayName("test-Display")
-                .password("P4ssword").build();
+        User user = createValidUser();
 
         restTestClient
                 .post()
@@ -40,12 +48,39 @@ public class UserControllerTest {
 
     @Test
     public void postUser_whenUserIsValid_userSavedToDatabase() {
+        User user = createValidUser();
 
+        testRestTemplate.postForEntity("/users", user, Object.class);
+
+        assertThat(userRepository.count()).isEqualTo(1);
+    }
+
+    @Test
+    public void postUser_whenUserIsValid_isHashedInDatabase(){
+        User user = createValidUser();
+
+        testRestTemplate.postForEntity("/users", user, Object.class);
+
+        List<User> users = userRepository.findAll();
+        User userDB = users.get(0);
+
+        assertThat(userDB.getPassword()).isNotEqualTo(user.getPassword());
+    }
+
+    @Test
+    public void postUser_whenUserHasNullUsername_receiveBadRequest() {
+        User user = createValidUser();
+        user.setUsername(null);
+
+        ResponseEntity<Object> response = testRestTemplate.postForEntity("/users", user, Object.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
     }
 
     private User createValidUser(){
-        return User.builder().username("test-user").displayName("test-Display").password("P4ssword").build();
+        return User.builder()
+                .username("test-user")
+                .displayName("test-Display")
+                .password("P4ssword").build();
     }
-
-
 }
